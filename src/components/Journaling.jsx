1c4f6ad5fcddfaf17bd1a1579b837819630cb69b
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-15 16:18:24 +0200

    add setMood buttons

--- src/components/Journaling.jsx
+++ src/components/Journaling.jsx
@@ -6,6 +6,15 @@ const Journaling = () => {
   const [entry, setEntry] = useState("");
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
+  const [mood, setMood] = useState("");
+  const feelings = [
+    { name: "Happiness", emoji: "😊" },
+    { name: "Sadness", emoji: "😢" },
+    { name: "Anger", emoji: "😠" },
+    { name: "Fear", emoji: "😨" },
+    { name: "Surprise", emoji: "😯" },
+    { name: "Disgust", emoji: "🤢" },
+  ];
 
   const handleAnalyze = async () => {
     if (!entry.trim()) return alert("Please enter a journal entry!");
@@ -23,19 +32,41 @@ const Journaling = () => {
 
   return (
     <div className="max-w-[1200px] mx-[auto] my-[50px] text-center">
-      <h1 className="text-[#4A90E2] text-2xl">
+      <h1 className="text-[#4A90E2] text-2xl mb-6">
         🌿 MindNest – Your Gentle Mood Journal
       </h1>
+      <h3>How you feel right now?</h3>
+      <div className="flex flex-wrap justify-center gap-4 p-6  rounded-xl shadow-lg mb-6">
+        {feelings.map(({ name, emoji }) => (
+          <button
+            key={name}
+            onClick={() => setMood(name)}
+            className="w-16 h-16 flex items-center justify-center text-2xl rounded-full border-2 border-blue-300 bg-white shadow-md transition-all duration-300 transform hover:scale-110 hover:bg-blue-200 focus:ring-4 focus:ring-blue-400 active:bg-blue-300"
+          >
+            {emoji}
+          </button>
+        ))}
+
+        <p className="mt-6 text-xl text-gray-700">
+          {mood
+            ? `You're feeling ${mood.toLowerCase()}`
+            : "How are you feeling today?"}
+        </p>
+      </div>
+      <h4 className="mb-6">
+        "Let your thoughts flow freely—this is a safe and gentle space for you
+        to express whatever is on your heart.💚
+      </h4>
       <textarea
         rows="4"
-        className="w-full p-[10px] rounded-[5px] border-[1px] border-solid border-[#ccc] text-[16px]"
+        className="w-full p-[10px] rounded-[5px] border-[1px] border-solid border-[#ccc] text-[16px] shadow-lg"
         placeholder="Write about your thoughts and feelings..."
         value={entry}
         onChange={(e) => setEntry(e.target.value)}
       />
       <br />
       <button
-        className="bg-[#4A90E2] text-[#fff] px-[15px] py-[10px] border-none rounded-[5px] cursor-pointer mt-[10px]"
+        className="bg-[#4A90E2] text-[#fff] px-[15px] py-[10px] border-none rounded-[5px] cursor-pointer mt-[10px] shadow-lg"
         onClick={handleAnalyze}
         disabled={loading}
       >
